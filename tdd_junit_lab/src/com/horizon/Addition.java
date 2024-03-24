2ac8da977a94b919630493bd9186e4c0f3bca43d
hjaiejhoussem <houssem.hjaiej@horizon-tech.tn>
2024-03-24 17:12:28 +0100

    [ADD] add testIllegalArgs method

--- tdd_junit_lab/src/com/horizon/Addition.java
+++ tdd_junit_lab/src/com/horizon/Addition.java
@@ -4,7 +4,7 @@ public class Addition {
 	
 	public Addition() {}
 
-	public int add(int i, int j) {
+	public int add(int i, int j){
 		if(i < 0 || j < 0) throw new NegativeIntegerException();
 		return i + j;
 	}
--- tdd_junit_lab/test/horizon/testing/calc/AdditionTest.java
+++ tdd_junit_lab/test/horizon/testing/calc/AdditionTest.java
@@ -2,7 +2,10 @@ package horizon.testing.calc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Method;
+
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.horizon.Addition;
@@ -10,12 +13,18 @@ import com.horizon.NegativeIntegerException;
 
 class AdditionTest {
 	
+	static Addition op; 
+	
+	@BeforeAll 
+	static void setUp() {
+		op = new Addition();
+	}
+	
 	//Case 1
 	//When adding 2 integers where one of them is negative
 	//Then display an error message
 	@Test
 	void testNegativeInt() {
-		Addition op = new Addition();
 		Assert.assertThrows(NegativeIntegerException.class,() -> op.add(1, -1));
 	}
 	
@@ -24,7 +33,6 @@ class AdditionTest {
 	//Then display the accurate result of the addition of the 2 inputs
 	@Test
 	void testAddTwoIntegers() {
-		Addition op =  new Addition();
 		Assert.assertEquals(2, op.add(1, 1));
 	}
 	
@@ -32,5 +40,10 @@ class AdditionTest {
 	//Case 3
 	//When adding 2 inputs where one of them (or both) are not integers
 	//Then display an error message
+	@Test
+	void testIllegalArgsType() throws NoSuchMethodException, SecurityException {
+		Method addMethod = op.getClass().getMethod("add", int.class, int.class);
+        Assert.assertThrows(IllegalArgumentException.class, () -> addMethod.invoke(op, 1, "a"));
+	}
 
 }
